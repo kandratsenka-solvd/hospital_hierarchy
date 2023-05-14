98771a67fd6f9d51f2577c598f63a9e2ecd01dd6
kandratsenka-solvd <akandratsenka.laba@solvd.com>
2023-05-14 13:21:53 +0200

    add: tests for getting data with reflection

---
+++ src/test/java/tests/ReflectionTest.java
@@ -0,0 +1,61 @@
+package tests;
+
+import org.testng.annotations.Test;
+import project.department.medical.MedicalDepartment;
+import java.lang.reflect.*;
+
+
+public class ReflectionTest {
+
+    @Test
+    public void testHospital() throws InvocationTargetException, IllegalAccessException {
+        Class<?> medicalDepartment = MedicalDepartment.class;
+        Field[] fields = medicalDepartment.getDeclaredFields();
+        Method[] methods = medicalDepartment.getDeclaredMethods();
+        Constructor<?>[] constructors = medicalDepartment.getDeclaredConstructors();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            System.out.printf("""
+                            "Field: %s"
+                            "Type: %s"
+                            "Modifiers: %s"
+                            %n""",
+                    field.getName(), field.getType().getName(), Modifier.toString(field.getModifiers()));
+        }
+
+        for (Constructor<?> constructor : constructors) {
+            constructor.setAccessible(true);
+            System.out.printf("""
+                            "Constructor: %s"
+                            "Modifiers: %s"
+                            "Parameters: %s"
+                            %n""",
+                    constructor.getName(), Modifier.toString(constructor.getModifiers()),
+                    Modifier.toString(constructor.getModifiers()));
+            Parameter[] parameters = constructor.getParameters();
+            for (Parameter parameter : parameters) {
+                System.out.println(parameter.getType().getName() + " " + parameter.getName() + "\n");
+            }
+        }
+
+        for (Method method : methods) {
+            method.setAccessible(true);
+            int modifiers = method.getModifiers();
+            System.out.printf("""
+                            "Modifier: %s"
+                            "Return type: %s"
+                            "Method name: %s"
+                            %n""",
+                    Modifier.toString(modifiers), method.getReturnType().getName(), method.getName());
+            Class<?>[] paramTypes = method.getParameterTypes();
+            for (Class<?> paramType : paramTypes) {
+                System.out.println(paramType.getName());
+            }
+            if (method.getName().equals("getDoctorsTotalNumber")) {
+                method.setAccessible(true);
+                method.invoke(null);
+            }
+        }
+    }
+}
