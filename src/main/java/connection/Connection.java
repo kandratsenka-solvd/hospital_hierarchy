c21a7709a1cc73bd5559f0773d1ebc00dcffaacf
kandratsenka-solvd <akandratsenka.laba@solvd.com>
2023-05-22 18:47:03 +0200

    add: action simulating method

--- src/main/java/connection/Connection.java
+++ src/main/java/connection/Connection.java
@@ -1,4 +1,18 @@
 package connection;
 
+import org.apache.logging.log4j.Logger;
+import utils.LoggerUtil;
+
 public class Connection {
+
+    private static final Logger LOGGER = LoggerUtil.getLogger();
+
+    public static void performAction() {
+        try {
+            LOGGER.info("Is performing some action...");
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
